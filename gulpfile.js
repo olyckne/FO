2b6a480bcb64fdc90acbebba9f99781f2357ab3e
Mattias Lyckne <mattias@lyckne.se>
2015-07-16 17:04:20 +0200

    Fix build issues

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ require('laravel-elixir-karma');
 
 elixir(function(mix) {
     mix.bower()
+        .styles(['../bower_components/bootstrap/dist/css/bootstrap.min.css'], 
+            'public/css/vendor.css'
+        )
         .scriptsIn('app', 'public/js/app.js')
         .karma({
             jsDir: ['app/**/*.js']
