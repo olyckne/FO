6779061a046a4cd2c88e14ecc43f386fd95ddf9e
Mattias Lyckne <mattias@lyckne.se>
2015-08-23 01:57:00 +0200

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var elixir = require('laravel-elixir');
-require('laravel-elixir-bower');
 require('laravel-elixir-karma');
 
+var scripts = [
+    '../../../bower_components/angular/angular.min.js',
+    '../../../bower_components/angular-ui-router/release/angular-ui-router.js',
+    '../../../bower_components/angular-sanitize/angular-sanitize.js',
+    '../../../bower_components/angular-bind-html-compile/angular-bind-html-compile.js',
+    '../../../bower_components/angular-animate/angular-animate.js'
+];
+
 elixir(function(mix) {
-    mix.bower()
-        .styles(['../bower_components/bootstrap/dist/css/bootstrap.min.css'], 
+    mix.scripts(scripts, 'public/js/vendor.js')
+       .styles(['../../../bower_components/bootstrap/dist/css/bootstrap.min.css'],
             'public/css/vendor.css'
         )
         .scriptsIn('app', 'public/js/app.js')
